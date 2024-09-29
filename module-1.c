128db3712b35d6938cde46ca542f76729c6caa2d
mdrobin45 <robinrana4r@gmail.com>
2024-09-29 22:24:13 +0600

    init

---
+++ module-1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/*int main() {
+   int num1,num2,result;
+   printf("Enter first number: \n");
+   scanf(" %d", &num1);
+   printf("Enter second number: \n");
+   scanf(" %d", &num2);
+
+   result = num1 + num2;
+   printf("Sum is %d \n",result);
+
+   result = num1 * num2;
+    printf("Multiply is %d \n",result);
+
+   result = num1 - num2;
+    printf("Subtraction is %d \n",result);
+
+   result = num1 / num2;
+    printf("Division is %d \n",result);
+   return 0;
+}
+*/
+int main(){
+   float a;
+   a = 4.254544454545454545454;
+   printf("%.20f", a);
+}
