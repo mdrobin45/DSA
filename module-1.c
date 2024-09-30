17894cfd928b306f32702a9492ccc41194dcfd30
mdrobin45 <robinrana4r@gmail.com>
2024-09-30 20:51:22 +0600

    find large number

--- module-1.c
+++ module-1.c
@@ -1,28 +1,27 @@
-#include <stdio.h>
+#include<stdio.h>
 
-/*int main() {
-   int num1,num2,result;
-   printf("Enter first number: \n");
-   scanf(" %d", &num1);
-   printf("Enter second number: \n");
-   scanf(" %d", &num2);
-
-   result = num1 + num2;
-   printf("Sum is %d \n",result);
-
-   result = num1 * num2;
-    printf("Multiply is %d \n",result);
-
-   result = num1 - num2;
-    printf("Subtraction is %d \n",result);
-
-   result = num1 / num2;
-    printf("Division is %d \n",result);
-   return 0;
-}
-*/
 int main(){
-   float a;
-   a = 4.254544454545454545454;
-   printf("%.20f", a);
+   int num1, num2, num3, big;
+
+   printf("Enter first number: \n");
+   scanf("%d", &num1);
+
+   printf("Enter second number: \n");
+   scanf("%d", &num2);
+
+   printf("Enter third number: \n");
+   scanf("%d", &num3);
+
+   if(num1>num2 && num1>num3)
+   {
+      big = num1;
+   } if(num2>num1 && num2>num3)
+   {
+      big = num2;
+   } if(num3>num1 && num3>num2){
+      big = num3;
+   }
+
+   printf("Largest number is: %d\n", big);
+   return 0;
 }
