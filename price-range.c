ae328f4b27e4a166222bed5a590f44ce0ef681ec
mdrobin45 <robinrana4r@gmail.com>
2024-10-24 23:23:44 +0600

    solved - price range

---
+++ price-range.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+   int n, start,end;
+   scanf("%d %d %d", &n, &start,&end);
+
+   int prices[n],count;
+
+   for (int i = 0; i < n; i++)
+   {
+      scanf("%d", &prices[i]);
+   }
+
+   for (int j = 0; j < n; j++)
+   {
+      if(prices[j]>=start && prices[j]<=end ){
+         count++;
+      }
+   }
+   printf("%d", count);
+
+   return 0;
+}
