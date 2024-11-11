be52fad65da7587b4bd53681f57e6fcecc607938
mdrobin45 <robinrana4r@gmail.com>
2024-11-11 07:38:37 +0600

    solve logn string

---
+++ long-string.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+   char* str[] = "Hello, world! I am learning C programming language. ^_^\nProgramming is fun and challenging. /\\/\\/\\ \nI want to give my 100% dedication to learn!\tI will succeed one day.";
+   printf("%s", str);
+   return 0;
+}
